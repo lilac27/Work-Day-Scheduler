97aa1fff082fceaf8207b305b777419ea91bcc85
Lilac <jonasjosh7@gmail.com>
2023-05-18 17:34:03 -0600

    retrieval from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -24,30 +24,42 @@ console.log(localStorage)
 localStorage.setItem(timeBlockId, value);
 })
 
-
-
-
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var time = dayjs().format('hh:mm');
+  if time == 
+
+
+
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  function gettodos() {
-    var todos = localStorage.getItem(value);
-    if (todos) {
-      todos = JSON.parse(todos);
-    } else {
-      todos = [];
-    }
-    return todos;
-  }
-  gettodos()
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 
   //
   // TODO: Add code to display the current date in the header of the page.
